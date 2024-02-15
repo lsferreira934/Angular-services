293b9719fe6e7e27fa6757f491c35e73c6874f7b
Leandro Ferreira <leandrosf934@gmail.com>
2024-02-15 19:25:09 -0300

    chore: config project

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+  @Input() pokemon: Pokemon | undefined;
+  error: string | null = null;
+
+  constructor(private pokemonService: PokemonService) {}
+}
